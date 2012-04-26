eb687ae3dd6c437500e7304826c009d6e1cc2d17
Jeff Su <me@jeffsu.com>
2012-04-26 10:19:47 -0700

    adding profiler

--- examples/server.js
+++ examples/server.js
@@ -2,16 +2,29 @@ var connect  = require('connect');
 var sidekick = require('../lib/sidekick');
 var http     = require('http');
 
+var SERVER_PORT   = 8888;
+var SIDEKICK_PORT = 8887;
+
 var sd  = sidekick.middleware();
 var app = connect.createServer();
 app.use(sd.connect());
-app.use(function (req, res, next) { res.end('hello'); });
-
-app.listen(8080);
-sd.listen(8079);
-
-var tailer = new sidekick.clients.Tailer('localhost', 8079);
-tailer.on('request', function (request) {
-  console.log(request.url);
+app.use(function (req, res, next) { 
+  setTimeout(function () { 
+    res.end("hello");
+  }, 200);
 });
+
+app.listen(SERVER_PORT);
+sd.listen(SIDEKICK_PORT);
+
+var tailer = new sidekick.clients.Tailer('localhost', SIDEKICK_PORT);
+tailer.on('request', function (request) { console.log(request.url); });
 tailer.start();
+
+var profiler = new sidekick.clients.Profiler('localhost', SIDEKICK_PORT);
+profiler.on('request', function (data) { 
+  console.log("Response Status: "    + data.response.statusCode); 
+  console.log("Response Time: "    + data.response.time); 
+  console.log("Response headers: " + data.response.headers); 
+});
+profiler.start();
